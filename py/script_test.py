ff33efb2f7f054aab1d13f4a27f03ee4cdfcabc8
Argyn Khuan <argynkhuan@yandex.kz>
2020-09-27 21:21:05 +0600

    test init

---
+++ py/script_test.py
@@ -0,0 +1,12 @@
+import os
+
+dirr = "./imgs"
+
+
+for file in os.listdir("./imgs"):
+    #if file.endswith(".txt"):
+    print (str(file))
+    
+    
+    # print(os.path.join("./igms", file))
+        
